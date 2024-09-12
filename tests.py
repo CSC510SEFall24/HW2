3a89fd7acbee25c3ccebdcae15b8363211b3a7d8
bhavishya <bhavishya2612@gmail.com>
2024-09-12 15:30:57 -0400

    adding workflows and tests

--- tests.py
+++ tests.py
@@ -1,16 +1,38 @@
+"""
+Tests for hw2_debugging
+"""
+import pytest
 from hw2_debugging import merge_sort
 
 def test1():
+    """
+    Test 1 for Merge Sort
+    Returns:
+        True if test passes
+        False if test fails
+    """
     arr = [10, 9, 77, 3, 5, 20, 35]
-    output = [3, 5, 9, 10, 20, 35, 77]
-    assert merge_sort(arr) == output
+    merge_sort(arr)
 
 def test2():
+    """
+    Test 2 for Merge Sort
+    Returns:
+        True if test passes
+        False if test fails
+    """
     arr = [100, 50, 89, 23, 1, 7, 255]
-    output = [1, 7, 23, 50, 89, 100, 255]
-    assert merge_sort(arr) == output
+    merge_sort(arr)
 
 def test3():
+    """
+    Test 3 for Merge Sort
+    Returns:
+        True if test passes
+        False if test fails
+    """
     arr = [15, 29, 2, 66, 34, 99, 40]
-    output = [2, 15, 29, 34, 40, 66, 99]
-    assert merge_sort(arr) == output
+    merge_sort(arr)
+
+if __name__ == "__main__":
+    pytest.main()
